cd0474ba34dd9027021a36818ed6fc110c5c0148
apprent <handehai@gmail.com>
2014-12-17 22:40:45 +0800

    Completed the makeCacheMatrix and the cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make cache matrix and solve its inverse matrix
 
-## Write a short comment describing this function
+## Make cache matrix and save its inverse matrix in cache
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## Set NULL to inverse matrix (im)
+        im <- NULL
+        
+        ## Set cached matrix and reset cached im
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        
+        ## Output matrix
+        get <- function() x
+        
+        ## Set cached inverse matrix to inputim
+        setim <- function(inputim) im <<- inputim
+        
+        ## Output inverse matrix
+        getim <- function() im
+        
+        ## Make the list
+        list(set = set, get = get,
+             setim = setim,
+             getim = getim)
 
 }
 
 
-## Write a short comment describing this function
+## Check if inverse matrix exist otherwise solve it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Get cached inverse matrix
+        im <- x$getim()
+        
+        ## Use cached im if not NULL
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        
+        ## Otherwise fetch the matrix
+        data <- x$get()
+        ## solve im
+        im <- solve(data, ...)
+        ## and put it in cache
+        x$setim(im)
+        
+        ## Output im
+        im
 }
